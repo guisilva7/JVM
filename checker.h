42131063dc63f24d50963ae1322675eaea8503c8
Ana Carolina dos Santos Pergentino <anacpergentino@MacBook-Pro-de-Ana.local>
2018-12-02 17:13:46 -0200

    métodos verifica bytecode e verifica constant pool

--- checker.h
+++ checker.h
@@ -3,9 +3,9 @@
 
 bool DescritorCampo(uint16_t, constant_pool_info *);
 bool DescritorMetodo(constant_pool_info *, uint16_t);
-void verifyConstantPool(ClassFile *);
+void VerificaConstantPool(ClassFile *);
 void AcessoFlags(ClassFile *);
-void VerBytecode(attribute_info *, ClassFile *);
+void VerificaBytecode(attribute_info *, ClassFile *);
 void ArquivoClassverificador(ClassFile *);
 void SuperVerificador(ClassFile *, JVM *);
 void VerificadorOverride(ClassFile *, JVM *);
