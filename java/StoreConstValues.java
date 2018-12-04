e2cd48c0607fd1fdf55c2c20f213fce1b42364a7
Ana Carolina dos Santos Pergentino <anacpergentino@MacBook-Pro-de-Ana.local>
2018-12-04 20:56:48 -0200

    arquivos teste

---
+++ java/StoreConstValues.java
@@ -0,0 +1,37 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+        double dvalue = 0.0d;
+        float fvalue  = 0.0f;
+        long lvalue = 0;
+        int ivalue = 0;
+        String svalue = "STRING TESTE VALUE";
+
+    	dvalue = 1.0d;
+    	fvalue  = 1.0f;
+    	lvalue = 1;
+    	ivalue = 1;
+    	dvalue = 1.1d;
+    	fvalue  = 1.1f;
+    	lvalue = 2;
+    	ivalue = 2;
+    	dvalue = 1.1175263d;
+    	fvalue  = 1.1234f;
+    	dvalue = 2.0d;
+    	fvalue  = 2.0f;
+    	dvalue = 3.0d;
+    	fvalue  = 3.0f;
+    	lvalue = 3;
+    	ivalue = 3;
+     	dvalue = 4.0d;
+    	fvalue  = 4.0f;
+
+        lvalue = 4;
+        ivalue = 4;
+
+        dvalue = 5.0d;
+        fvalue  = 5.0f;
+
+        lvalue = 5;
+        ivalue = 5;
+    }
+}
